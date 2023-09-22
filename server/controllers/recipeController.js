d3cc0e7be5d6090acd5fb1f7383e7e4d47f07129
lawrencejews <lawrencejews1@gmail.com>
2023-09-22 23:36:43 +0900

    created models and added data to mongoDB

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,6 +1,56 @@
-// HomePage
+require("../models/database");
+const Category = require("../models/Category");
 
+// HomePage
 exports.homepage = async(req, res) => {
 
-  res.render("index", {title: "Cooking Blog - Home"});
+  try {  
+
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+
+    res.render("index", {title: "Cooking Blog - Home", categories});
+  } catch (error) {
+    res.status(500).send({message: error.message || "Error on HomePage"})
+  }
+
 }
+
+//=====================================================Testing Section for Data Insert into MongoDB===============================================
+// async function insertDummyData() {
+//   try {
+//     await Category.insertMany(
+//       [
+//       {
+//         "name": "Thai",
+//         "image": "thai-food.jpg"
+//       },
+//       {
+//         "name": "American",
+//         "image": "american-food.jpg"
+//       }, 
+//       {
+//         "name": "Chinese",
+//         "image": "chinese-food.jpg"
+//       },
+//       {
+//         "name": "Mexican",
+//         "image": "mexican-food.jpg"
+//       }, 
+//       {
+//         "name": "Indian",
+//         "image": "indian-food.jpg"
+//       },
+//       {
+//         "name": "Spanish",
+//         "image": "spanish-food.jpg"
+//       }
+//     ]
+//     );
+
+//   } catch (error) {
+//     console.log("error", + error)
+//   }
+// }
+
+// insertDummyData()
